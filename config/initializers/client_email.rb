574e169cda3153391977d2b4b3dad23304b812da
johndavid400 <johndavid400@yahoo.com>
2011-12-12 16:59:32 -0600

    [CLEAN UP CODE]

--- config/initializers/client_email.rb
+++ config/initializers/client_email.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   :port                 => 587,
   :domain               => 'gmail.com',
   :user_name            => 'isotope.eleven@gmail.com',
-  :password             => 'boobies11',
+  :password             => 'XrscDlnSLMNNKLDNDLflk',
   :authentication       => 'plain',
   :enable_starttls_auto => true
 }
