0185359ce355ba63615b9fc2be45e5187ffc83a6
johndavid400 <johndavid400@yahoo.com>
2011-12-12 15:32:13 -0600

    [MAILERS] set up first mailer, needs to be renamed

--- app/controllers/client_applications_controller.rb
+++ app/controllers/client_applications_controller.rb
@@ -1,6 +1,4 @@
 class ClientApplicationsController < ApplicationController
-  # GET /applications
-  # GET /applications.json
   def index
     @applications = ClientApplication.all
 
@@ -10,8 +8,6 @@ class ClientApplicationsController < ApplicationController
     end
   end
 
-  # GET /applications/1
-  # GET /applications/1.json
   def show
     @application = ClientApplication.find(params[:id])
 
@@ -21,40 +17,22 @@ class ClientApplicationsController < ApplicationController
     end
   end
 
-  # GET /applications/new
-  # GET /applications/new.json
   def new
     @application = ClientApplication.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @application }
-    end
   end
 
-  # GET /applications/1/edit
   def edit
     @application = ClientApplication.find(params[:id])
   end
 
-  # POST /applications
-  # POST /applications.json
   def create
     @application = ClientApplication.new(params[:application])
-
-    respond_to do |format|
-      if @application.save
-        format.html { redirect_to home_finished_path, notice: 'Application was successfully created.' }
-        format.json { render json: @application, status: :created, location: @application }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @application.errors, status: :unprocessable_entity }
-      end
+    if @application.save
+      Initial.initial_email(@application).deliver
     end
+    redirect_to home_finished_path
   end
 
-  # PUT /applications/1
-  # PUT /applications/1.json
   def update
     @application = ClientApplication.find(params[:id])
 
@@ -69,8 +47,6 @@ class ClientApplicationsController < ApplicationController
     end
   end
 
-  # DELETE /applications/1
-  # DELETE /applications/1.json
   def destroy
     @application = ClientApplication.find(params[:id])
     @application.destroy
--- app/models/client_application.rb
+++ app/models/client_application.rb
@@ -1,2 +1,7 @@
 class ClientApplication < ActiveRecord::Base
+#  after_create :send_initial_email
+
+  def send_initial_email
+    Initial.initial_email(@application).deliver
+  end
 end
