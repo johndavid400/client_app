40191f6dde2ac6bc5bd08388b8f0b89fd7387bac
johndavid400 <johndavid400@yahoo.com>
2012-01-04 16:12:18 -0600

    [RSPEC] added rspec tests to check models using shoulda matchers for valid database relationships

--- spec/models/banking_information_form_spec.rb
+++ spec/models/banking_information_form_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
 describe BankingInformationForm do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:client_application) }
 end
--- spec/models/principal_information_form_spec.rb
+++ spec/models/principal_information_form_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
 describe PrincipalInformationForm do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:client_application) }
 end
