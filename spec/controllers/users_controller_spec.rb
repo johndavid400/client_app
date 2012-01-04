371afff187580694b09dfbaf37c38f554108c206
johndavid400 <johndavid400@yahoo.com>
2012-01-04 16:11:18 -0600

    [USERS SPEC] removed unused page actions from spec tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,18 +9,4 @@ describe UsersController do
     end
   end
 
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'show'" do
-    it "returns http success" do
-      get 'show'
-      response.should be_success
-    end
-  end
-
 end
