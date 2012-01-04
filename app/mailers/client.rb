f9d6ebe8fa00709472f923c886fb8188cd86b8d7
johndavid400 <johndavid400@yahoo.com>
2012-01-04 13:42:39 -0600

    [MAILER] added mailer actions to send to both isotope and client

--- app/mailers/client.rb
+++ app/mailers/client.rb
@@ -3,7 +3,18 @@ class Client < ActionMailer::Base
 
   def client_email(application)
     @application = application
-    @url  = client_application_path(@application)
-    mail(:to => "jd@isotope11.com", :subject => "You have received a new client application")
+    @url = client_application_path(@application)
+    mail(:to => @application.email, :subject => "Correspondence with Isotope11")
+  end
+
+  def isotope_email(application)
+    @application = application
+    mail(:to => "jd@isotope11.com", :subject => "A client has submitted information to Isotope11.")
+  end
+
+  def completed_email(application)
+    @application = application
+    mail(:to => "jd@isotope11.com", :subject => "A client has completed their application.")
+    mail(:to => @application.email, :subject => "You have completed your application with Isotope11.")
   end
 end
