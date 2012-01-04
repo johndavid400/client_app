17598d6994621076a5b1ffa0afc7d29c4143303f
johndavid400 <johndavid400@yahoo.com>
2012-01-04 16:13:08 -0600

    [RSPEC] client_application spec needed to have some tests added to it

--- spec/models/client_application_spec.rb
+++ spec/models/client_application_spec.rb
@@ -1,12 +1,15 @@
 require 'spec_helper'
 
 describe ClientApplication do
+  it { should validate_uniqueness_of(:email) }
+  it { should validate_uniqueness_of(:business_name) }
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:business_name) }
   it { should_not allow_value("tom.tom.com").for(:email) }
   it { should allow_value("tom@tom.com").for(:email) }
 
   it { should belong_to(:user) }
+  it { should belong_to(:state) }
   it { should have_many(:attachments) }
   it { should have_many(:principal_information_forms) }
   it { should have_many(:banking_information_forms) }
