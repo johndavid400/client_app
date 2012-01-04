c3063c336606c90807e8451bfe2e4874849274cf
johndavid400 <johndavid400@yahoo.com>
2012-01-04 16:14:06 -0600

    [RSPEC] changed the way Rspec initializes so it will clean the database after each test and create a client application for the uniqueness validations to pass.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,7 +27,17 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
+
+  config.before :each do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.start
+    ClientApplication.create!(:email => 'john@john.com', :business_name => "John's plumbing")
+  end
+
+  config.after do
+    DatabaseCleaner.clean
+  end
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
