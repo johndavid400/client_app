d3833a43221585e52bd3e40652283b408200c1aa
johndavid400 <johndavid400@yahoo.com>
2012-03-08 09:11:25 -0600

    updated mailer info

--- app/mailers/client.rb
+++ app/mailers/client.rb
@@ -5,17 +5,17 @@ class Client < ActionMailer::Base
     @client_application = application
     @url = client_application_path(@client_application)
     mail(:to => @client_application.email, :subject => "Correspondence with Isotope11")
-    mail(:to => "jd@isotope11.com", :subject => "A client has submitted information to Isotope11.")
+    mail(:to => "clientapp@isotope11.com", :subject => "A client has submitted information to Isotope11.")
   end
 
   def isotope_email(application)
     @client_application = application
-    mail(:to => "jd@isotope11.com", :subject => "A client has submitted information to Isotope11.")
+    mail(:to => "clientapp@isotope11.com", :subject => "A client has submitted information to Isotope11.")
   end
 
   def completed_email(application)
     @client_application = application
-    mail(:to => "jd@isotope11.com", :subject => "A client has completed their application.")
+    mail(:to => "clientapp@isotope11.com", :subject => "A client has completed their application.")
     mail(:to => @client_application.email, :subject => "You have completed your application with Isotope11.")
   end
 end
